bd16f945812c6def8f4b707e8db84a314ed3c6ec
davidshare <davidessienshare@gmail.com>
2018-12-01 06:09:36 +0100

    bug(webpackExport): fix issues with exports
    - change the export command from module.export to module.exports
      in the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.export = {
+module.exports = {
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, '/dist'),
